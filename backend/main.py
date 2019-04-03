7bc5114f8f48995bf0c84e04f7056783ecf738ef
Kevin Gao <kevingao2003@gmail.com>
2019-04-02 21:54:23 -0400

    backend data pushing works

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,27 @@
-import firebase_admin 
-from firebase_admin import firestore
+import pyrebase
 
-#from firebase_admin import credentials, firestore
-#
-#
-#
-#cred = credentials.Certificate('backend/ServiceAccountKey.json')
-#default_app = firebase_admin.initialize_app(cred)
-#db = firestore.client()
+config = {
+    "authDomain": "orderly-life.firebaseapp.com",
+    "apiKey": "AIzaSyAB5gvYSeK8U4KSKlBcPRADDVyWi4dLsMg",
+    "databaseURL": "https://orderly-life.firebaseio.com",
+    "projectId": "orderly-life",
+    "storageBucket": "orderly-life.appspot.com",
+    "serviceAccount": "backend/AccountServiceKey.json"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+auth = firebase.auth()
+user = auth.sign_in_with_email_and_password("kevingao2003@gmail.com", "Gaoww8922")
+
+info = auth.get_account_info(user['idToken'])
+#print(info)
+
+db = firebase.database()
+
+data = {
+    "name": "Mortimer 'Morty' Smith"
+}
+
+results = db.child("users").set(data)
+
