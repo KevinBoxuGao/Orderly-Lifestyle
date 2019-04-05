9c426cfe376d01d96f9535e9588f2e5041b41300
Kevin Gao <kevingao2003@gmail.com>
2019-04-04 22:50:56 -0400

    basic login page

---
+++ frontend/js/fireBase.js
@@ -0,0 +1,11 @@
+$(function(){
+    var config = {
+        apiKey: "AIzaSyAB5gvYSeK8U4KSKlBcPRADDVyWi4dLsMg",
+        authDomain: "orderly-life.firebaseapp.com",
+        databaseURL: "https://orderly-life.firebaseio.com",
+        projectId: "orderly-life",
+        storageBucket: "orderly-life.appspot.com",
+        messagingSenderId: "957471703508"
+    };
+    firebase.initializeApp(config);
+});  
